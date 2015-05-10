67349f7968e3c85d8e760b619f5c5de556f86d83
Satomi <stm.ngw@gmail.com>
2015-05-10 18:57:18 +0900

    add colum status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425055047) do
+ActiveRecord::Schema.define(version: 20150510095230) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150425055047) do
     t.integer  "sort"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "status"
   end
 
 end
